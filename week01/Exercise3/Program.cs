b6f0da9c8b346b862b145883bcb06229a989fe68
Ilegubo Kofi Owusu <226963023+Ilegubo@users.noreply.github.com>
2025-11-01 17:42:51 +0000

    W01 Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-        int digit_guess;
+        int digit_guess, number_of_tries=0;
         string guess;
         Random randomGenerator = new Random();
         int magic_number = randomGenerator.Next(1, 100);
@@ -15,6 +15,8 @@ class Program
         {
             Console.Write("What is your guess? "); guess = Console.ReadLine();
             digit_guess = int.Parse(guess);
+            number_of_tries += 1;
+            Console.WriteLine($"Number of attempts {number_of_tries}");
             {
                 if (digit_guess < magic_number)
                 {
