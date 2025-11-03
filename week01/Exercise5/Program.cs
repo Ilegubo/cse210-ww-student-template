9700137e9d5149de102157283b21f693f1b3003e
Ilegubo Kofi Owusu <226963023+Ilegubo@users.noreply.github.com>
2025-11-03 10:07:56 +0000

    W01 Exercise 05 Corrected the function 'DisplayResult' to have return type of void

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,4 +6,35 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? "); string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? "); string number = Console.ReadLine();
+        int _number = int.Parse(number);
+
+        return _number;
+    }
+
+    static int SquareNumber(int base_number)
+    {
+        return base_number * base_number;
+    }
+    
+    static void DisplayResult()
+    {
+        Console.Write("What is your name? "); string name = Console.ReadLine();
+        Console.Write("Enter a square: "); string square = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name} and the square number is {square}");
+    }
 }
